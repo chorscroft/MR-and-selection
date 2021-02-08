f6477bd2a504ab473221401df97fb4b3347e5fdf
Clare Horscroft <chorscroft@aol.co.uk>
2021-02-08 17:33:59 +0000

    Tidied up code

--- Simulations/children/MRinR_children.R
+++ Simulations/children/MRinR_children.R
@@ -9,13 +9,9 @@ dat<-read.table("output.txt",header = TRUE)
 par<-read.table("parent.txt",header = TRUE)
 
 dat$children<-sapply(dat$index,function(x) sum(par$parent1==x | par$parent2==x))
-dat$childrenOE<-dat$children/2
-
-barplot(table(dat$children),space = 0)
-lines(0:10+0.5,dpois(0:10,2)*10000,col="blue")
 
 sf<-systemfit(fit~pheno,inst = ~geno,method = "2SLS",data=dat)
 summary(sf)
 
-sf_children<-systemfit(childrenOE~pheno,inst = ~geno,method = "2SLS",data=dat)
+sf_children<-systemfit(children~pheno,inst = ~geno,method = "2SLS",data=dat)
 summary(sf_children)
