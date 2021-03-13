7cf0694a6db2869877a07cf03fd48015d1366b83
Clare Horscroft <clare.horscroft@bristol.ac.uk>
2021-03-13 00:04:08 +0000

    Added some summary tables, changed filepath

--- Simulations/basic/basic.R
+++ Simulations/basic/basic.R
@@ -3,8 +3,14 @@ require(systemfit)
 ################################################################
 ## Basic simulation, 0.05(0.01)
 ################################################################
-setwd("~/Bristol/Simulations/basic")
+setwd("~/Documents/MR-and-selection/Simulations/basic")
 
 dat<-read.table("output.txt",header = TRUE)
+table(dat$geno)
+tapply(dat$pheno,dat$geno,mean)
+tapply(dat$fit,dat$geno,mean)
+plot(dat$pheno,dat$fit)
+summary(lm(dat$fit~dat$pheno))
+
 sf<-systemfit(fit~pheno,inst = ~geno,method = "2SLS",data=dat)
 summary(sf)
--- Simulations/two_pheno/two_pheno.R
+++ Simulations/two_pheno/two_pheno.R
@@ -1,12 +1,19 @@
 require(systemfit)
 
 ################################################################
-## Two phenotype simulation, 0.05(0.01), -0.03(0.01), additive
+## Two phenotype simulation, 0.05, -0.03, additive
 ################################################################
-setwd("~/Bristol/Simulations/two_pheno/")
+setwd("~/Documents/MR-and-selection/Simulations/two_pheno/")
 
-dat2<-read.table("output.txt",header = TRUE)
-sf1<-systemfit(fit~pheno1+pheno2,inst = ~geno,method = "2SLS",data=dat2)
+dat<-read.table("output.txt",header = TRUE)
+table(dat$geno)
+tapply(dat$pheno1,dat$geno,mean)
+tapply(dat$pheno2,dat$geno,mean)
+tapply(dat$fit,dat$geno,mean)
+
+sf1<-systemfit(fit~pheno1+pheno2,inst = ~geno,method = "2SLS",data=dat)
 summary(sf1)
-sf2<-systemfit(fit~pheno2,inst = ~geno,method = "2SLS",data=dat2)
+sf2<-systemfit(fit~pheno1,inst = ~geno,method = "2SLS",data=dat)
 summary(sf2)
+sf3<-systemfit(fit~pheno2,inst = ~geno,method = "2SLS",data=dat)
+summary(sf3)
