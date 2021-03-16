f46b3e9f8e16ee2138f55ee5ce5d60b54e049673
Clare Horscroft <clare.horscroft@bristol.ac.uk>
2021-03-16 19:09:42 +0000

    Init commit

--- master_script.sh
+++ master_script.sh
@@ -18,3 +18,22 @@ cd /Users/ba20701/Documents/MR-and-selection/Simulations/children
 slim children.txt
 Rscript createdatfile.R $PWD --vanilla
 open -a Rstudio MRinR_children.R
+
+## Run a children simulation with a large number of individuals
+
+cd /Users/ba20701/Documents/MR-and-selection/Simulations/children/children_large_n
+slim children_large_n.txt
+Rscript /Users/ba20701/Documents/MR-and-selection/Simulations/children/createdatfile.R $PWD --vanilla
+
+## Run the children simulation a large number of times 
+
+cd /Users/ba20701/Documents/MR-and-selection/Simulations/children/multisim
+for (i in 1..1000)
+do
+	mkdir sim_$i
+	cp children.txt sim_$i/children.txt
+	cd sim_$i
+	slim children.txt
+	Rscript /Users/ba20701/Documents/MR-and-selection/Simulations/children/createdatfile.R $PWD --vanilla
+	cd ..
+done
