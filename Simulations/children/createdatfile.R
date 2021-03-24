a26a0d4ba875822f6767762270d2c9752fb794e7
Clare Horscroft <clare.horscroft@bristol.ac.uk>
2021-03-24 22:33:06 +0000

    Made function more efficient

--- Simulations/children/createdatfile.R
+++ Simulations/children/createdatfile.R
@@ -4,7 +4,10 @@
 create_dat_file<-function(workingdir){
   dat<-read.table(paste0(workingdir,"/output.txt"),header = TRUE)
   par<-read.table(paste0(workingdir,"/parent.txt"),header = TRUE)
-  dat$children<-sapply(dat$index,function(x) sum(par$parent1==x)+sum(par$parent2==x))
+  temp<-as.data.frame(table(c(par$parent1,par$parent2)))
+  colnames(temp)<-c("index","children")
+  dat<-merge(dat,temp,all.x = TRUE)
+  dat$children[is.na(dat$children)]<-0
   write.table(dat,paste0(workingdir,"/dat.txt"),quote=FALSE,row.names = FALSE)
 }
 ## runs the function given a directory
